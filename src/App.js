076bad3ea1a00867569131c5be54e9793a680863
joellawrence <lawrencej43@mymacewan.ca>
2023-05-06 14:03:00 -0600

    Add Personal Projects section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from "./components/Contact"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
 import Skills from "./components/Skills"
+import PersonalProjects from "./components/PersonalProjects"
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
             <Navbar />
             <About />
             <Projects />
+            <PersonalProjects />
             <Skills />
             <Contact />
         </main>
